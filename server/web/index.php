a07996e62dcb28bf9e363a1821e915fc2c111462
vlt. Mondanzo <sackboysteve1@gmail.com>
2016-06-14 17:58:51 +0200

    added MYSQL SUPPORT!

--- server/web/index.php
+++ server/web/index.php
@@ -17,20 +17,19 @@
 				<h1>SKAP</h1>
 				<h6>SmartKey Administration Panel</h6>
 				<?php
-					$datab = mysqli_connect('localhost', 'smarktey', '123456', 'smartkey') or die("Can't Connect!");
-					$result	 = mysqli_query($db, "SELECT * FROM `skap`") or die("Can't execute query!");
-					echo <<< EOT
-<table border="1">
-<tr>
-<th>Datum/Zeit</th>
-<th>Bild</th>
-<th>Fehlgeschlagen</th>
-</tr>
-EOT;
+					$datab = mysqli_connect('5.230.10.116', 'smartkey', '123456', 'smartkey') or die("Can't Connect!");
+					$result	 = mysqli_query($datab, "SELECT * FROM `skap`") or die("Can't execute query!");
+					echo '
+							<table border="1">
+								<tr>
+									<th>Datum/Zeit</th>
+									<th>Bild</th>
+									<th>Fehlgeschlagen</th>
+								</tr>';
 					while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {						
 					  echo "<tr>";
 					  echo "<td>". $line['time'] . "</td>";
-					  echo "<td>". $line['img_url'] . "</td>";
+					  if (!empty($line['img_url'])) {echo "<td>". $line['img_url'] . "</td>";}
 					  echo "<td>". $line['failed'] . "</td>";
 					  echo "</tr>";
 					}
@@ -39,38 +38,6 @@ EOT;
 					mysqli_free_result($result);
 
 				?>
-				<table>
-					<tr>
-							<th>Datum/Zeit</th>
-							<th>Bild</th>
-							<th>Fehlgeschlagen</th>
-				</tr>
-					<tr>
-						<td>7.3.2015, 20:54:32</td>
-						<td><a href="/07.03.2015-20:54:32.png.jpg">172.16.105.242/07.03.2015-20:54:32.png</a></td>
-						<td>True</td>
-					</tr>
-					<tr>
-						<td>21.5.2016, 12:31:01</td>
-						<td><a href="">172.16.105.242/21.05.2016-12:31:01.png</a></td>
-						<td>False</td>
-					</tr>
-					<tr>
-						<td>12.6.2016, 13:02:31</td>
-						<td><a href="">172.16.105.242/12.06.2016-13:02:31.png</a></td>
-						<td>False</td>
-					</tr>
-					<tr>
-						<td>12.6.2016, 13:06:59</td>
-						<td><a href="">172.16.105.242/12.06.2016-13:06:59.png</a></td>
-						<td>False</td>
-					</tr>
-					<tr>
-						<td>12.6.2016, 13:07:44</td>
-						<td><a href="">172.16.105.242/12.06.2016-13:07:44.png</a></td>
-						<td>True</td>
-					</tr>
-				</table>
       </div>
     </div>
   </body>
